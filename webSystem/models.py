5efef09311b2e45ce4ee4cbf540fdcbf8a86b986
Diana_pwf <Diana_pwf@163.com>
2020-09-08 18:49:14 +0800

    加入设备和租借申请模型

--- webSystem/models.py
+++ webSystem/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 from enum import Enum
 # Create your models here.
-
-
+from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
+
+
+class EquipmentStatus(Enum):
+    # 已添加
+    EXIST = 1
+    # 等待批准上架
+    WAIT_ON_SHELF = 2
+    # 已上架
+    ON_SHELF = 3
+    # 等待批准借出
+    WAIT_ON_LOAN = 4
+    # 已借出
+    ON_LOAN = 5
 
 
 class SystemUser(AbstractUser):
@@ -14,3 +26,29 @@ class SystemUser(AbstractUser):
     # 权限在接口处通过对User属性判断即可
 
 
+class ApplicationStatus(Enum):
+    # 发出待审核
+    SENTED=1
+    # 正在进行
+    ON_PROCESS=2
+    # 未通过审核
+    NOT_PASS=3
+    # 租期结束
+    RENT_END=4
+
+
+class Equipment(models.Model):
+    status = models.PositiveSmallIntegerField(default=EquipmentStatus.EXIST)
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey('SystemUser', on_delete=models.CASCADE)
+    borrower = models.ForeignKey('SystemUser', on_delete=models.SET_DEFAULT, default='')
+    loan_end_time = models.DateTimeField(default=timezone.now)
+
+
+class LoanApplication(models.Model):
+    status = models.PositiveSmallIntegerField(default=ApplicationStatus.SENTED)
+    loaner = models.ForeignKey('SystemUser', on_delete=models.CASCADE)
+    applicant = models.ForeignKey('SystemUser', on_delete=models.CASCADE)
+    equipment = models.ForeignKey('Equipment', on_delete=models.CASCADE)
+    loan_start_time = models.DateTimeField(default=timezone.now)
+    loan_end_time = models.DateTimeField(default=timezone.now)

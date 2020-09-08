cee48b439893b4eeafabf8f7d7551450beb7336c
Diana_pwf <Diana_pwf@163.com>
2020-09-08 20:17:12 +0800

    实现注册接口

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webSystem import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/logon', views.logon_request),
 ]
--- webSystem/views.py
+++ webSystem/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse, JsonResponse
+from .models import SystemUser
+
+
+def logon_request(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+
+        if username == '' or password == '' or email == '':
+            return HttpResponse(status=400, content='invalid parameters')
+        elif SystemUser.objects.filter(username=username).exists():
+            return HttpResponse(status=400, content='user exists')
+        else:
+            user = SystemUser()
+            user.username = username
+            user.set_password(password)
+            user.save()
+            return HttpResponse(status=200)
+    else:
+        return HttpResponse(status=400, content='require POST')

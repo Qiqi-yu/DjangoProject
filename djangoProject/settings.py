989e8f84936e0f7002aa65a4186b4534a7ed2ac1
yqq18 <48144399+yqq18@users.noreply.github.com>
2020-09-08 18:44:51 +0800

    初始化用户模型

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ SECRET_KEY = '3b&4lkov7zo#n+hcv&vpz+xid(&l-#cq*n6&a9o02gxndawysb'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -81,7 +78,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -101,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -115,8 +110,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'webSystem.SystemUser'
--- webSystem/models.py
+++ webSystem/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from enum import Enum
 # Create your models here.
+
+
+from django.contrib.auth.models import AbstractUser
+
+
+class SystemUser(AbstractUser):
+    # 对User属性的判断
+    is_student = models.BooleanField(default=False)
+    is_provider = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    # 权限在接口处通过对User属性判断即可
+
+
